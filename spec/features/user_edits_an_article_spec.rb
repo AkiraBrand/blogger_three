a19b0e700e6e944dfa041b15e63e074beccf43e1
Ruth Brand <ruthbrandsoprano@gmail.com>
2019-08-21 08:44:18 -0700

    drying up edit functionality wip

---
+++ spec/features/user_edits_an_article_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'user can edit an existing article' do
+  describe 'they link from the articles index' do
+    describe 'they edit the title and body' do
+      it 'edits the article' do
+         article_1 = Article.create!(title: "Title 1", body: "Body 1")
+
+         visit article_path(article_1)
+
+         click_link "Edit"
+
+         expect current_path.to eq(edit_article_path)
+         fill_in "article[title]", with: "Old Title"
+         fill_in "article[body]", with: "Old Body"
+
+         click_on "Submit"
+
+         expect(page).to have_content "Old Title"
+         expect(page).to have_content "Old Body"
+       end
+    end
+  end
+end
